d6bca3d5102d7103b17a657b265984650b52c5c4
Axel Guilmin <axel@pixowl.com>
2013-02-25 08:58:22 +0100

    Initial Commit

---
+++ ThirDimension/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ThirDimension
+//
+//  Created by Axel on 25/02/13.
+//  Copyright (c) 2013 Axel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
